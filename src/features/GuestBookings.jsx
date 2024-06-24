eb56a15b4b10fa3026941b68f5b529ea378b58c7
John Raymond <john.raymond.blando@gmail.com>
2024-06-24 11:27:25 +0800

    Code cleanup

--- src/features/GuestBookings.jsx
+++ src/features/GuestBookings.jsx
@@ -1,12 +1,15 @@
 import React, { useState, useEffect  } from "react";
 import { deleteBooking, fetchAllBookings } from "../integrations/GuestBookings";
-import { Link, useLocation, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 export function GuestBookings() {
-    const location = useLocation()
+    const now = new Date()
+    const currentYear = Intl.DateTimeFormat('en', { year: "numeric"}).format(now)
+    const currentMonth = Intl.DateTimeFormat('en', { month: "numeric"}).format(now)
+    const currentDate = `${currentYear}-${(currentMonth.length > 1) ? currentMonth : `0${currentMonth}`}`;
 
     const [bookings, setBookings] = useState([])
-    const [searchDate, setSearchDate] = useState(location.state?.searchDate)
+    const [searchDate, setSearchDate] = useState(currentDate)
 
     useEffect(() => {
         fetchAllBookings(setBookings, searchDate)
@@ -30,6 +33,7 @@ export function GuestBookings() {
     const handleUpdateBooking = (bookingId) => {
         navigate(`/update/${bookingId}`, { state: { searchDate }})
     }
+
     return (
         <section className="guestbooking">
             <header>
@@ -37,41 +41,43 @@ export function GuestBookings() {
                 <label>Search Date</label>
                 <input type="month" onChange={handleOnChange} value={searchDate} />
             </header>
-            <button className="newbooking" onClick={handleNewBooking}>Add new booking</button>
-            <ul className="guestbookinglist">
-                {bookings.map(booking => {
-                    const checkInDate = new Intl.DateTimeFormat('en', {
-                        dateStyle: 'full',
-                      }).format(new Date(booking.checkIn))
-                    const checkOutDate = new Intl.DateTimeFormat('en', {
-                        dateStyle: 'full',
-                        }).format(new Date(booking.checkOut))
-                    const datePaid = new Intl.DateTimeFormat('en', {
-                        dateStyle: 'full',
-                        }).format(new Date(booking.datePaid))
-                    const amountFormatter = new Intl.NumberFormat("PH", {
-                        style: "currency",
-                        currency: "PHP",
-                        });
-                    return (
-                        <li key={booking._id} className="guestbookingitem">
-                            <article className="guestbookingdetails">
-                                <h3>{booking.guestName}</h3>
-                                <p>Booked in {booking.rooms.join(" and ")} from {booking.from}, {booking.noOfPax} pax, {booking.noOfStay} night/s of stay for a nightly price of {amountFormatter.format(booking.nightlyPrice)}</p>
-                                <p>The guest checked-in {checkInDate} <input type="date" readOnly value={booking.checkIn.split("T")[0]} /> and checked-out {checkOutDate} <input type="date" readOnly value={booking.checkOut.split("T")[0]} />.</p>
-                                {
-                                (booking.remarks.toLowerCase().includes('confirmed')) 
-                                    ? <p>The guest confirmed payment using {booking.modeOfPayment} as mode of payment on {datePaid} <input type="date" readOnly value={booking.datePaid.split("T")[0]} /> for a total of {amountFormatter.format(booking.totalPayout)}.</p>
-                                    : <p>The guest have not confirmed on payment yet</p>
-                                }
-                                <button className="delete" onClick={() => handleDelete(booking._id)}>Delete booking</button>
-                                <button className="updatebooking" onClick={() => handleUpdateBooking(booking._id)}>Update booking</button>
-                            </article>
-                        </li>
-                    )
-                })}
-            </ul>
-            <button className="newbooking" onClick={handleNewBooking}>Add new booking</button>
+            <section>
+                <button className="newbooking" onClick={handleNewBooking}>Add new booking</button>
+                <ul className="guestbookinglist">
+                    {bookings.map(booking => {
+                        const checkInDate = new Intl.DateTimeFormat('en', {
+                            dateStyle: 'full',
+                        }).format(new Date(booking.checkIn))
+                        const checkOutDate = new Intl.DateTimeFormat('en', {
+                            dateStyle: 'full',
+                            }).format(new Date(booking.checkOut))
+                        const datePaid = new Intl.DateTimeFormat('en', {
+                            dateStyle: 'full',
+                            }).format(new Date(booking.datePaid))
+                        const amountFormatter = new Intl.NumberFormat("PH", {
+                            style: "currency",
+                            currency: "PHP",
+                            });
+                        return (
+                            <li key={booking._id} className="guestbookingitem">
+                                <article className="guestbookingdetails">
+                                    <h3>{booking.guestName}</h3>
+                                    <p>Booked in {booking.rooms.join(" and ")} from {booking.from}, {booking.noOfPax} pax, {booking.noOfStay} night/s of stay for a nightly price of {amountFormatter.format(booking.nightlyPrice)}</p>
+                                    <p>The guest checked-in {checkInDate} <input type="date" readOnly value={booking.checkIn.split("T")[0]} /> and checked-out {checkOutDate} <input type="date" readOnly value={booking.checkOut.split("T")[0]} />.</p>
+                                    {
+                                    (booking.remarks.toLowerCase().includes('confirmed')) 
+                                        ? <p>The guest confirmed payment using {booking.modeOfPayment} as mode of payment on {datePaid} <input type="date" readOnly value={booking.datePaid.split("T")[0]} /> for a total of {amountFormatter.format(booking.totalPayout)}.</p>
+                                        : <p>The guest have not confirmed on payment yet</p>
+                                    }
+                                    <button className="delete" onClick={() => handleDelete(booking._id)}>Delete booking</button>
+                                    <button className="updatebooking" onClick={() => handleUpdateBooking(booking._id)}>Update booking</button>
+                                </article>
+                            </li>
+                        )
+                    })}
+                </ul>
+                <button className="newbooking" onClick={handleNewBooking}>Add new booking</button>
+            </section>
         </section>
     )
 }
